216fae8649d1724505d803201fc6f95d4c796a0e
walkure <1270667+walkure@users.noreply.github.com>
2024-10-25 23:31:17 +0900

    add SetLogger

--- wosensor.go
+++ wosensor.go
@@ -1,9 +1,19 @@
 package wosensors
 
-import "github.com/walkure/gatt"
+import (
+	"log/slog"
+
+	"github.com/walkure/gatt"
+	"github.com/walkure/gatt/logger"
+)
 
 // The Member Service UUID of Woan Technology (Shenzhen) Co., Ltd.
 var memberUUID = gatt.MustParseUUID("fd3d")
 
 // The Company Identifier of Woan Technology (Shenzhen) Co., Ltd.
 const companyID = 0x0969
+
+// SetLogger sets the logger for the package
+func SetLogger(newLogger *slog.Logger) {
+	logger.SetLogger(newLogger)
+}
